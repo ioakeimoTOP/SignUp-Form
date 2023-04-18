6aff40173e88f90024c2593ffea0a1d291334ebc
ioakeimo <ioakeimo.dev@gmail.com>
2023-04-18 17:35:23 +0300

    Improve form validation visuals
    After an initial round of user feedback, the need for more subtle error
    hints that only appear reactively to user input was expressed.

--- src/scripts/formValidation.js
+++ src/scripts/formValidation.js
@@ -12,10 +12,18 @@ function validatePasswordPattern(password, pattern) {
 
 function updateValidityList(passwordField) {
   const password = passwordField.value;
+  const validationTitle = document.getElementById('validation__all');
   const validationList = document.querySelector('.validation');
 
   // Handle complete validity aria:
-  validationList.ariaHidden = passwordField.validity.valid ? 'true' : 'false';
+  const completeValidity = passwordField.validity.valid;
+  if (completeValidity) {
+    validationList.ariaHidden = 'true';
+    validationTitle.classList.add('validation__criterion-satisfied');
+  } else {
+    validationList.ariaHidden = 'false';
+    validationTitle.classList.remove('validation__criterion-satisfied');
+  }
 
   for (const [id, pattern] of Object.entries(validationPatterns)) {
     let element = validationList.querySelector('#' + id);
@@ -50,11 +58,23 @@ confirmPassword.addEventListener('input', (e) => {
   }
 });
 
-// Handle Submit:
 const form = document.getElementById('signup-form');
+
+// Handle Submit:
 function onFormSubmit(e) {
   // Do nothing (can be implemented in future iterations)
   e.preventDefault();
   location.reload();
 }
 form.addEventListener('submit', onFormSubmit);
+
+// Attach user filled input classes
+// Use single listener with event delegation
+form.addEventListener('input', (e) => {
+  const interactedInputField = e.target;
+  if (e.target.value) {
+    e.target.closest('.signup__field').classList.add('signup__field-filled');
+  } else {
+    e.target.closest('.signup__field').classList.remove('signup__field-filled');
+  }
+});
